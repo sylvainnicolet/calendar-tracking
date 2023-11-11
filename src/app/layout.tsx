9df9f6e9ceeef0cdff81fee8a94b60b6ae63c9e7
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-11-11 19:26:50 +0100

    suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute='class'
